25f4ba87a99fddc3b033143c277e852801e95b38
William Weber <weber.williamb@gmail.com>
2019-06-05 11:14:34 -0700

    Wokflow Updated

--- gruntfile.js
+++ gruntfile.js
@@ -4,24 +4,65 @@ module.exports = function (grunt) {
 
     //Project and task configuration
 
-    const sass = require('node-sass');
-
-    require('load-grunt-tasks')(grunt);
-
+    const sass= require('node-sass')
+    
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        /**
+         * Grunt Sass
+         * Compile Sass to CSS using node-sass
+         * https://www.npmjs.com/package/grunt-sass
+         */
+
         sass: {
             options: {
                 implementation: sass,
-                sourceMap: true
+                sourceMap: false
             },
             dist: {
                 files: {
                     'CSS/styles.css': 'assets/scss/styles.scss'
                 }
             }
+        },
+        /**
+         * Grunt Contrib Watch
+         * Monitor files and excute tasks
+         * https://www.npmjs.com/package/grunt-contrib-watch
+         */
+
+        watch: {
+            sass: {
+                files: [
+                    'assets/scss/*.scss'
+                ],
+                tasks: [
+                    'sass'
+                ]
+            },
+            scripts: {
+                files: [
+                    'assets/js/*.js'
+                ],
+                tasks: [
+                    'uglify'
+                ]
+            }
+        },
+        
+        uglify: {
+            my_target:{
+                files:{
+                    'JS/scripts.js' : ['assets/js/scripts.js', 'node_modules/jquery/dist/jquery.js']
+                }
+            }
         }
+
     });
 
-    grunt.registerTask('default', ['sass']);
+    require('load-grunt-tasks')(grunt);
+
+    grunt.registerTask('default', ['watch']);
 
 };
